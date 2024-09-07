03c563eaf5b07a98143e9b43fb627d5a411bc388
shubham19032004 <shubhamnainwal70@gmail.com>
2024-09-08 00:07:04 +0530

    cat-file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
-use std::fs;
+use std::{fs, io::Read};
+use flate2::read::ZlibDecoder;
 use std::env;
 fn main() {
-    println!("Hello, world!");
     let args:Vec<String>=env::args().collect();
     if args[1]=="init"{
         fs::create_dir(".vc").unwrap();
-        fs::create_dir(".vc/object").unwrap();
+        fs::create_dir(".vc/objects").unwrap();
         fs::create_dir(".vc/refs").unwrap();
         fs::write(".vc/HEAD", "ref:/heads/master\n").unwrap();
         println!("Initialized the vc directory");
-    }else{
+    }
+        else if args[1]=="cat-file" {
+            if args[2]=="-p"{
+                let hash=args[3].clone();
+                let folder_name=&hash[0..2];
+                let file_name=&hash[2..]; 
+                let path=format!(".git/objects/{folder_name}/{file_name}");
+
+                let mut object=fs::File::open(&path).unwrap();
+                let mut content:Vec<u8>=vec![];
+
+                let mut extracted_content=String::new();
+
+                object.read_to_end(&mut content).unwrap();
+
+                let mut decoder = ZlibDecoder::new(content.as_slice());
+
+               decoder.read_to_string(&mut extracted_content).unwrap();
+                print!("{extracted_content}");
+
+            }
+    }
+    else{
         println!("unkown command:{}",args[1])
     }
-
 }
